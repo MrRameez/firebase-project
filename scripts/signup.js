794b0bf2b67da189b674af1b3cd4e6d4a90646d5
MrRameez <156177291+MrRameez@users.noreply.github.com>
2024-07-30 12:42:11 +0500

    Update signup.js

--- scripts/signup.js
+++ scripts/signup.js
@@ -13,6 +13,8 @@ button.addEventListener("click", async (event) => {
     
         await createUserWithEmailAndPassword(auth, emailValue, passwordValue);
 
+     window.location.herf = "../Pages/dashboard"
+
         // Optionally redirect or show success message here
     } catch (error) {
         Swal.fire({
